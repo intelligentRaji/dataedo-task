07fa20901441fd664b2e84bacabde3e9839b3b8d
intelligentRaji <sergdim7@gmail.com>
2024-03-11 14:56:48 +0100

    feat(sad): asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core'
+import { Component, Injector, OnInit } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+
+import { ChildComponent } from './child/child.component'
+import { SimpleService } from './servicies/simple.service'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [SimpleService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'angular-setup'
+
+  constructor(private injector: Injector) {}
+
+  public ngOnInit(): void {
+    console.log(this)
+  }
 }
